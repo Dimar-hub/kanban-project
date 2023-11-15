2c5f13a9a052a7358bc06a0810f8f7c17ce5ddaf
Dimar-hub <dimarzioagressa-student2021@smktelkom-lpg.sch.id>
2023-11-14 16:33:32 -0800

    Develop TaskList View

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'Selamat Datang';
+    return 'home';
 });
+
+
+Route::get('/tasks/', [TaskController::class, 'index']); // Ditambahkan
